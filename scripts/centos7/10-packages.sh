64d87acdc2b56326eb9fe41369101caf8d4c60cb
G. Panula <github@kablah.com>
2020-11-03 16:36:39 -0600

    :zap: tweak the package removal bit

--- scripts/centos7/10-packages.sh
+++ scripts/centos7/10-packages.sh
@@ -5,6 +5,11 @@
 yum -y install @core authconfig audit deltarpm sudo chrony cloud-init cloud-utils-growpart dracut-config-generic dracut-norescue firewalld grub2 kernel rsync tar yum-utils tuned gnutls gdisk bind-utils tcpdump
 
 # Remove the packages we don't want or need in our AWS base image
+rpm -qa | grep NetworkManager | xargs yum -y remove
+rpm -qa | grep alsa | xargs yum -y remove
+rpm -qa | grep firmware | xargs yum -y remove
+yum -y remove iprutils plymouth postfix --setopt="clean_requirements_on_remove=1"
+
 # install aws agents
 yum -y install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 yum -y install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm

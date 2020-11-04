9f07870f4d5b90df0409815a5d98863975633ee9
G. Panula <github@kablah.com>
2020-11-04 10:49:54 -0600

    :wrench: adjust grun cmdline linux

--- scripts/centos7/60-grub.sh
+++ scripts/centos7/60-grub.sh
@@ -11,14 +11,14 @@ source /tmp/env_vars
 echo 'force_drivers+="ena virtio ixgbevf"' >/etc/dracut.conf.d/01_aws_modules.conf
 
 # Drop default config for grub
-cat > /etc/default/grub << EOT
+cat >/etc/default/grub <<EOT
 GRUB_TIMEOUT=1
 GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
 GRUB_DEFAULT=saved
 GRUB_DISABLE_SUBMENU=true
 GRUB_TERMINAL="serial console"
 GRUB_SERIAL_COMMAND="serial --speed=115200"
-GRUB_CMDLINE_LINUX="console=ttyS0,115200 console=tty0 vconsole.font=latarcyrheb-sun16 crashkernel=auto vconsole.keymap=us plymouth.enable=0 net.ifnames=0 biosdevname=0"
+GRUB_CMDLINE_LINUX="console=tty0 crashkernel=auto console=ttyS0,115200 audit=1 rd.plymouth=0 plymouth.enable=0 net.ifnames=0 biosdevname=0"
 GRUB_DISABLE_RECOVERY="true"
 EOT
 
